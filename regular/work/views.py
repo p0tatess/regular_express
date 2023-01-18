4ce7aa3de655b40403eac9ef9b4c54c4b68b372c
p0tatess <p0tates322@gmail.com>
2023-01-18 20:02:37 +0700

    add other buttons

--- regular/work/views.py
+++ regular/work/views.py
@@ -14,9 +14,18 @@ def index(request):
     if  form.is_valid():
         lining = form.cleaned_data.get('line')
         regularexpress = form.cleaned_data.get('regular_express')
-        regexp = re.search(regularexpress,lining)
+        if submitbutton == 're.search':
+            regexp = re.search(regularexpress,lining)
+        elif submitbutton == 're.match':
+            regexp = re.match(regularexpress,lining)
+        elif submitbutton == 're.findall':
+            regexp = re.findall(regularexpress,lining)
+        else:
+            regexp = re.split(regularexpress,lining)
+
 
     context = {
         'form': form, 'lining': lining, 'regularexpress': regularexpress, 'submitbutton': submitbutton,'regexp':regexp,
     }
+
     return render(request,'work/index.html',context)
